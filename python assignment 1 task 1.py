2b3dc3e62e9d7bdf9f5c8dc1734d78374a7b8d03
TaherKaru <TAHERKARU53@GMAIL.COM>
2025-05-20 05:47:55 -0700

    uploading files

---
+++ python assignment 1 task 1.py
@@ -0,0 +1,11 @@
+a=input("enter first number: ")
+a=float(a)
+b=input("enter second number: ")
+b=float(b)
+#addition
+print("addition: ",a+b)
+print("subtraction: ",a-b)
+print("multiplication: ",a*b)
+print("division: ",a/b)
+
+
---
+++ python assignment 1 task 2.py
@@ -0,0 +1,4 @@
+a=input("enter your first name: ")
+b=input("enter your surname: ")
+fullname=a + b
+print("hello, "+fullname+"! welcome to the python world")
